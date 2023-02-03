42cc7290d9a8323e435bc0b450c864e2f5813575
danchang11 <cssource@livemail.tw>
2023-02-04 01:16:46 +0800

    handle both send fail and receive fail

--- Client.c
+++ Client.c
@@ -16,10 +16,13 @@ int getSecondsOfFloatSeconds(float); //get the number of seconds.
 int getMicroSecondsOfFloatSeconds(float); // get the number of microseconds.
 void setTimeout(struct timeval*, int , int ); // set the time interval
 void setSocketOption(int*, struct timeval*); // set the option of socket.
+void handleFailed(const int , int * ,struct timeval *, int ); //handle when sending is failed.
+
+static char* fileName;
 
 int main(int argc, char **argv)
 {
-    char* fileName = argv[0];
+    fileName = argv[0];
     if(argc < 2 || argc > 3)
     {
         e_ErrorType error = Input;
@@ -55,10 +58,6 @@ int main(int argc, char **argv)
     setSocketOption(&SocketFD, &timeout);
 
 
-
-    float multiplier = 2.f;
-    float baseWaitInterval = 0.5;
-    int maxWaitInterval = 8;
     int isSent = -1;
     int isReceived = -1;
     int lowerThanMaxRetry;
@@ -67,9 +66,10 @@ int main(int argc, char **argv)
     int SentTimes = 0;
     while(isReceived < 0 && lowerThanMaxRetry){
         isSent = sendto(SocketFD, SendingBuffer, bufferSize, 0, (struct sockaddr*) &IPaddress, sizeof(IPaddress));
+        SentTimes++;
         if(isSent < 0){
-            e_ErrorType error = Send;
-            errorHandler(fileName, error);
+            handleFailed(SentTimes, &lowerThanMaxRetry, &timeout, SocketFD);
+            continue;
         }
         printf("[+]Data sent : %s\n", SendingBuffer);
 
@@ -80,20 +80,13 @@ int main(int argc, char **argv)
         // < 0 means it's failed.
         if( isReceived < 0 )
         {
-            float newInterval = getNewWaitInterval(SentTimes, baseWaitInterval, multiplier, maxWaitInterval);
-            int Seconds = getSecondsOfFloatSeconds(newInterval);
-            int microSeconds = getMicroSecondsOfFloatSeconds(newInterval);
-
-            setTimeout(&timeout, Seconds, microSeconds);
-            setSocketOption(&SocketFD, &timeout);
-            printf("[-]Message sending Failed!, Resending it after %f Seconds...\n", newInterval);
-
-            lowerThanMaxRetry = getIsLowerThanMaxRetry(); // Check if it is still lower than Max re-try times
+            handleFailed(SentTimes, &lowerThanMaxRetry, &timeout, SocketFD);
+            continue;
         }
         else{
             printf("[+]Data received: %s\n", ReceivingBuffer);
         }
-        SentTimes++;
+
     }
 
     if(isReceived < 0)
@@ -117,7 +110,7 @@ int getIsLowerThanMaxRetry()
 
 float getNewWaitInterval(int n, float baseInterval, float multiplier, float maxWaitInterval )
 {
-    float waitInterval = baseInterval * pow(multiplier,n);
+    float waitInterval = baseInterval * pow(multiplier,n-1);
     if(waitInterval > maxWaitInterval) return maxWaitInterval;
     return waitInterval;
 }
@@ -145,6 +138,27 @@ void setSocketOption(int *SocketFD, struct timeval* timeout)
 {
     if(setsockopt(*SocketFD, SOL_SOCKET, SO_RCVTIMEO,timeout,sizeof(*timeout))<0){
         e_ErrorType error = SetSocketOpt;
-        errorHandler("Client", error);
+        errorHandler(fileName, error);
     }
+
+    if(setsockopt(*SocketFD, SOL_SOCKET, SO_SNDTIMEO,timeout,sizeof(*timeout))<0){
+        e_ErrorType error = SetSocketOpt;
+        errorHandler(fileName, error);
+    }
+}
+
+void handleFailed(const int SentTimes, int *lowerThanMaxRetry ,struct timeval *timeout, int SocketFD)
+{
+    float multiplier = 2.f;
+    float baseWaitInterval = 0.5;
+    int maxWaitInterval = 8;
+    float newInterval = getNewWaitInterval(SentTimes, baseWaitInterval, multiplier, maxWaitInterval);
+    int Seconds = getSecondsOfFloatSeconds(newInterval);
+    int microSeconds = getMicroSecondsOfFloatSeconds(newInterval);
+
+    setTimeout(timeout, Seconds, microSeconds);
+    setSocketOption(&SocketFD, timeout);
+    printf("[-]Message sending Failed!, Resending it after %f Seconds...\n", newInterval);
+
+    *lowerThanMaxRetry = getIsLowerThanMaxRetry(); // Check if it is still lower than Max re-try times
 }
